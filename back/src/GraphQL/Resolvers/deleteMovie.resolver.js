872d3594b644ee71c07df0549e46d655f5f51f5d
mohamed tarek <mohamad1tarek1@gmail.com>
2024-12-14 23:52:26 +0200

    integeration redis and graphql

--- back/src/GraphQL/Resolvers/deleteMovie.resolver.js
+++ back/src/GraphQL/Resolvers/deleteMovie.resolver.js
@@ -1,5 +1,7 @@
 import { Movie } from "../../../DB/models/index.js";
 import { isAuthQL } from "../Middlewares/authentication.middleware.js";
+//redis
+import { client } from "../../../src/Redis/redis.js";
 export const deleteMovie = async (parent, args) => {
   const { id,token } = args;
   //=====================authentication================
@@ -15,5 +17,11 @@ export const deleteMovie = async (parent, args) => {
   if (!deletedMovie) {
     return new Error("there is no matched movie");
   }
+
+  const key = "movies";
+  const firstthirtyMovies = await Movie.find().limit(30);
+  console.log(firstthirtyMovies);
+  await client.set(key, JSON.stringify(firstthirtyMovies));
+
   return "movie deleted successfully";
 };
--- back/src/GraphQL/Resolvers/getMovies.resolver.js
+++ back/src/GraphQL/Resolvers/getMovies.resolver.js
@@ -1,5 +1,18 @@
 import { Movie } from "../../../DB/models/index.js";
+//redis
+import { client } from "../../../src/Redis/redis.js";
 export const getMovies = async () => {
-    const movies=await Movie.find();
-    return movies;
+    const key = "movies";
+      const moviesFromCache = await client.get(key);
+      const moviesParsed = JSON.parse(moviesFromCache);
+      if(!moviesParsed){
+        return next(
+          new ErrorClass(
+            "there is no movies in cache",
+            400,
+            "there is no movies cache" 
+          )
+        );
+      }
+    return moviesParsed;
 }
